8986bc070c7e0f7b00c751757e23ec920351d39e
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-16 16:29:36 +0300

    Configure open API specs in the main entry script

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
+
+    const config = new DocumentBuilder()
+        .setTitle("Beauty Tags API")
+        .setDescription("API that returns beauty tags to a user.")
+        .setVersion("1.0")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
+
     await app.listen(5000);
 }
 
