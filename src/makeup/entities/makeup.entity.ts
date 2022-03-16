72e835ffd69c4090557024f043ccac4915860202
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-16 16:37:15 +0300

    Add makeup database entity

---
+++ src/makeup/entities/makeup.entity.ts
@@ -0,0 +1,17 @@
+import { BaseEntity, Column, CreateDateColumn, Entity, Index, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Makeup extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Index()
+    @Column({ name: "name", type: "varchar", length: 50 })
+    name: string;
+
+    @CreateDateColumn({ name: "created_at", type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: "updated_at", type: "timestamptz", default: () => "CURRENT_TIMESTAMP" })
+    updatedAt: Date;
+}
