be5304020552b93d48407ff03e1df0a96ba10d27
Arnold Kunihira <arnold.kmulera@gmail.com>
2022-03-16 19:52:19 +0300

    Enable Cross Origin Resource sharing

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe());
+    app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+    app.enableCors();
 
     const config = new DocumentBuilder()
         .setTitle("Beauty Tags API")
